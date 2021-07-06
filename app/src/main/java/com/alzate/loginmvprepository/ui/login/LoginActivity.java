24f6842b9d07e098656e2d85fe1f5423089e8a93
AntonioAlzate <antonioalzate7@gmail.com>
2021-07-05 21:39:16 -0500

    Initial commit

---
+++ app/src/main/java/com/alzate/loginmvprepository/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.alzate.loginmvprepository.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alzate.loginmvprepository.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
