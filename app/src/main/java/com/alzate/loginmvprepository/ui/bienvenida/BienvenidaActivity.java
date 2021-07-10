588961209d8bb9d081d1d7cba5a81075b2f48929
AntonioAlzate <antonioalzate7@gmail.com>
2021-07-09 23:16:55 -0500

    En BienvenidaActivity ahora se reciben los datos enviados desde el login y se muestran en pantalla.

--- app/src/main/java/com/alzate/loginmvprepository/ui/bienvenida/BienvenidaActivity.java
+++ app/src/main/java/com/alzate/loginmvprepository/ui/bienvenida/BienvenidaActivity.java
@@ -3,14 +3,39 @@ package com.alzate.loginmvprepository.ui.bienvenida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.alzate.loginmvprepository.R;
+import com.alzate.loginmvprepository.dto.InformacionUsuarioDTO;
+import com.alzate.loginmvprepository.ui.login.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BienvenidaActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtNombreEnviado)
+    public TextView txtNombreEnviado;
+    @BindView(R.id.txtIdentificacionEnviado)
+    public TextView txtIdentificacionEnviada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+        ButterKnife.bind(this);
+
+        cargarInformacionEnviada();
+    }
+
+    private void cargarInformacionEnviada() {
+        Bundle objetoEnviado = getIntent().getExtras();
+        InformacionUsuarioDTO infoUsuario;
+
+        if(objetoEnviado != null){
+            infoUsuario = (InformacionUsuarioDTO) objetoEnviado.getSerializable(LoginActivity.EXTRA_INFO_USUARIO);
+            txtNombreEnviado.setText(infoUsuario.getNombre());
+            txtIdentificacionEnviada.setText(infoUsuario.getIdentificacion());
+        }
     }
 }
